20a95f2a1f2f75d447b3497c56e61f1ffd2823b2
Julian Zalewski <julzal2000@gmail.com>
2024-02-23 00:20:13 +0100

    🥰🥰🥰

---
+++ src/OOFM.Ui/PingService.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Hosting;
+using OOFM.Core.Api;
+using System.Net.Http;
+
+namespace OOFM.Ui;
+
+//GZD http connection keepalive
+internal class PingService(IHttpClientProvider httpClientProvider) : BackgroundService
+{
+    protected async override Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            using (var msg = new HttpRequestMessage(HttpMethod.Head, "https://open.fm/api"))
+            {
+                await httpClientProvider.GetHttpClient().SendAsync(msg);
+            }
+
+            await Task.Delay(30_000);
+        }
+    }
+}
