8afa4bfa4f85bcb363a959741134cd2157d01085
Julian Zalewski <julzal2000@gmail.com>
2024-02-23 14:50:56 +0100

    Moved navigation events to the navigation namespace.

--- src/OOFM.Ui/Navigation/Events/NextButtonEventTrigger.cs
+++ src/OOFM.Ui/Navigation/Events/NextButtonEventTrigger.cs
@@ -1,15 +1,15 @@
 ﻿using Microsoft.Xaml.Behaviors;
 using System.Windows.Input;
 
-namespace OOFM.Ui.Events;
-internal class BackButtonEventTrigger : EventTrigger
+namespace OOFM.Ui.Navigation.Events;
+internal class NextButtonEventTrigger : EventTrigger
 {
     protected override void OnEvent(EventArgs eventArgs)
     {
         if (eventArgs is not MouseButtonEventArgs mouseEventArgs)
             return;
 
-        if (mouseEventArgs.ChangedButton == MouseButton.XButton1)
+        if (mouseEventArgs.ChangedButton == MouseButton.XButton2)
             InvokeActions(eventArgs);
     }
 }
