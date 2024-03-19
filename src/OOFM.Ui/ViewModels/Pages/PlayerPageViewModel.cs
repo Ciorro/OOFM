2a05fac24bffd135af4e6a3e8b6b2946f6c50e88
Ciorro <julzal2000@gmail.com>
2024-03-19 22:58:50 +0100

    Added player page.

---
+++ src/OOFM.Ui/ViewModels/Pages/PlayerPageViewModel.cs
@@ -0,0 +1,10 @@
+﻿using OOFM.Ui.Attributes;
+using OOFM.Ui.Navigation;
+
+namespace OOFM.Ui.ViewModels.Pages
+{
+    [PageKey("player")]
+    internal class PlayerPageViewModel : INavigationPage
+    {
+    }
+}
---
+++ src/OOFM.Ui/Views/Pages/PlayerPageView.xaml.cs
@@ -0,0 +1,10 @@
+﻿using System.Windows.Controls;
+namespace OOFM.Ui.Views.Pages;
+
+public partial class PlayerPageView : UserControl
+{
+    public PlayerPageView()
+    {
+        InitializeComponent();
+    }
+}
