d851f2c5dad7c361c2f0619604dfbfa08d3d506e
Ciorro <julzal2000@gmail.com>
2024-03-21 12:03:32 +0100

    Player page design.

--- src/OOFM.Ui/ViewModels/Pages/PlayerPageViewModel.cs
+++ src/OOFM.Ui/ViewModels/Pages/PlayerPageViewModel.cs
@@ -1,10 +1,52 @@
-﻿using OOFM.Ui.Attributes;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using OOFM.Core.Models;
+using OOFM.Ui.Attributes;
 using OOFM.Ui.Navigation;
+using OOFM.Ui.Radio;
+using OOFM.Ui.Services;
 
 namespace OOFM.Ui.ViewModels.Pages
 {
     [PageKey("player")]
-    internal class PlayerPageViewModel : INavigationPage
+    internal partial class PlayerPageViewModel : ObservableObject, INavigationPage
     {
+        private readonly IRadioService _radioService;
+        private readonly IPlaylistService _playlistService;
+
+        [ObservableProperty]
+        private Station? _station;
+
+        [ObservableProperty]
+        private Playlist? _playlist;
+
+        public PlayerPageViewModel(IRadioService radioService, IPlaylistService playlistService)
+        {
+            _radioService = radioService;
+            _playlistService = playlistService;
+        }
+
+        public void OnResumed()
+        {
+            Station = _radioService.CurrentStation;
+
+            if (Station is not null)
+            {
+                _playlistService.Subscribe(Station.Id, OnPlaylistUpdated);
+                Playlist = _playlistService.GetCurrentPlaylist(Station.Id);
+            }
+        }
+
+        public void OnPaused()
+        {
+            if (Station is not null)
+            {
+                _playlistService.Unsubscribe(Station.Id, OnPlaylistUpdated);
+            }
+        }
+
+        private void OnPlaylistUpdated(Playlist playlist)
+        {
+            Playlist = playlist;
+        }
     }
 }
--- src/OOFM.Ui/Views/Pages/PlayerPageView.xaml.cs
+++ src/OOFM.Ui/Views/Pages/PlayerPageView.xaml.cs
@@ -3,8 +3,25 @@ namespace OOFM.Ui.Views.Pages;
 
 public partial class PlayerPageView : UserControl
 {
+    const int BreakpointWidth = 768;
+
     public PlayerPageView()
     {
         InitializeComponent();
+        SizeChanged += OnSizeChanged;
+    }
+
+    private void OnSizeChanged(object sender, System.Windows.SizeChangedEventArgs e)
+    {
+        if (ActualWidth < BreakpointWidth)
+        {
+            SimilarChannels.Visibility = System.Windows.Visibility.Collapsed;
+        }
+        else
+        {
+            SimilarChannels.Visibility = System.Windows.Visibility.Visible;
+        }
+
+        SimilarChannels.Width = ActualWidth * 0.4;
     }
 }
