69da79eaf27253e9ddc6abfe3e48623d429805a0
Ciorro <julzal2000@gmail.com>
2024-04-25 16:42:49 +0200

    Removed unused methods from the PlaylistController.

--- src/OOFM.Core/Api/Controllers/IPlaylistController.cs
+++ src/OOFM.Core/Api/Controllers/IPlaylistController.cs
@@ -1,11 +1,7 @@
 ﻿using OOFM.Core.Api.Models;
+namespace OOFM.Core.Api.Controllers;
 
-namespace OOFM.Core.Api.Controllers
+public interface IPlaylistController
 {
-    public interface IPlaylistController
-    {
-        Task<Playlist> GetPlaylistById(int id, CancellationToken cancellationToken = default);
-        Task<IDictionary<int, Playlist>> GetPlaylistsByIds(IEnumerable<int> ids, CancellationToken cancellationToken = default);
-        Task<IDictionary<int, Playlist>> GetAllPlaylists(CancellationToken cancellationToken = default);
-    }
+    Task<IDictionary<int, Playlist>> GetAllPlaylists(CancellationToken cancellationToken = default);
 }
--- src/OOFM.Core/Api/Controllers/PlaylistController.cs
+++ src/OOFM.Core/Api/Controllers/PlaylistController.cs
@@ -1,52 +1,41 @@
 ﻿using OOFM.Core.Api.Models;
 using System.Text.Json;
 
-namespace OOFM.Core.Api.Controllers
+namespace OOFM.Core.Api.Controllers;
+
+public class PlaylistController : IPlaylistController
 {
-    public class PlaylistController : IPlaylistController
+    private readonly IApiClient _client;
+    private readonly JsonSerializerOptions _jsonOptions;
+
+    public PlaylistController(IApiClient apiClient)
     {
-        private readonly IApiClient _client;
-        private readonly JsonSerializerOptions _jsonOptions;
+        _client = apiClient;
 
-        public PlaylistController(IApiClient apiClient)
+        _jsonOptions = new JsonSerializerOptions
         {
-            _client = apiClient;
+            PropertyNameCaseInsensitive = true
+        };
+    }
 
-            _jsonOptions = new JsonSerializerOptions
+    public async Task<IDictionary<int, Playlist>> GetAllPlaylists(CancellationToken cancellationToken = default)
+    {
+        var content = await _client.Request($"/radio/playlist", cancellationToken);
+
+        using (var ms = new MemoryStream(content))
+        {
+            var json = await JsonDocument.ParseAsync(ms);
+            var playlists = new Dictionary<int, Playlist>();
+
+            foreach (var jObj in json.RootElement.EnumerateObject())
             {
-                PropertyNameCaseInsensitive = true
-            };
-        }
-
-        public async Task<Playlist> GetPlaylistById(int id, CancellationToken cancellationToken = default)
-        {
-            return (await GetAllPlaylists(cancellationToken))[id];
-        }
-
-        public async Task<IDictionary<int, Playlist>> GetPlaylistsByIds(IEnumerable<int> ids, CancellationToken cancellationToken = default)
-        {
-            return (await GetAllPlaylists(cancellationToken)).Where(p => ids.Contains(p.Key)).ToDictionary();
-        }
-
-        public async Task<IDictionary<int, Playlist>> GetAllPlaylists(CancellationToken cancellationToken = default)
-        {
-            var content = await _client.Request($"/radio/playlist", cancellationToken);
-
-            using (var ms = new MemoryStream(content))
-            {
-                var json = await JsonDocument.ParseAsync(ms);
-                var playlists = new Dictionary<int, Playlist>();
-
-                foreach (var jObj in json.RootElement.EnumerateObject())
+                if (int.TryParse(jObj.Name, out var id))
                 {
-                    if (int.TryParse(jObj.Name, out var id))
-                    {
-                        playlists.Add(id, jObj.Value.Deserialize<Playlist>(_jsonOptions)!);
-                    }
+                    playlists.Add(id, jObj.Value.Deserialize<Playlist>(_jsonOptions)!);
                 }
-
-                return playlists;
             }
+
+            return playlists;
         }
     }
 }
